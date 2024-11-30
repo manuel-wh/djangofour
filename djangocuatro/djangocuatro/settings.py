049639385f9baf20fa19e27b81e406e9c1a1bcd5
Manuel Mukul <manuel@wisphub.net>
2024-11-30 11:58:35 -0500

    hot fix settings_server

--- djangocuatro/djangocuatro/settings.py
+++ djangocuatro/djangocuatro/settings.py
@@ -155,3 +155,10 @@ STATICFILES_DIRS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
+try:
+    # Try import settings_server.py for local purpose.
+    from settings_server import *
+except ImportError:
+    # Doesn't matter if settings_server.py not exist.
+    pass
--- djangocuatro/djangocuatro/settings_server.py
+++
@@ -1,6 +0,0 @@
-try:
-    # Try import settings_server.py for local purpose.
-    from settings_server import *
-except ImportError:
-    # Doesn't matter if settings_server.py not exist.
-    pass
